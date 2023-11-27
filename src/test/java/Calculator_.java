883c60be2feeb77d7c1966ad0d735b56450fd489
jevora <legendus@gmail.com>
2023-11-27 12:37:06 +0000

    uploading tests

---
+++ src/test/java/Calculator_.java
@@ -0,0 +1,13 @@
+import org.example.Calculator;
+import org.junit.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class Calculator_ {
+
+	@Test
+	public void foo() {
+		Calculator calculator = new Calculator();
+		assertThat(calculator.sum(2, 4)).isEqualTo(6);
+	}
+}
